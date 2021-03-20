c3530f812d9f097b64e865889fe9a2474582d018
김지수 <jisu31438@gmail.com>
2021-03-21 01:21:20 +0900

    message

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
-const { User } = require("./models/User");
+const { auth } = require('./middleware/auth')
+const { User } = require("./config/models/User");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,7 +23,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!천사와아이들화이팅!!!!')
 })
 
-app.post('/register', (req, res) => {
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요 ~")
+})
+
+app.post('/api/users/register', (req, res) => {
 
   //회원가입 할 때 필요한 정부들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
@@ -39,7 +42,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
 
@@ -64,11 +67,40 @@ app.post('/login', (req, res) => {
           res.cookie("x_auth", user.token)
           .status(200)
           .json({ loginSuccess: true, userId: user._id })
-        })
+      })
     })
-  })
-  
+  })  
 })
+
+//role 0 ->일반유저 role 0이 아니면 관리자 
+app.get('/api/users/auth', auth , (req, res) => {
+
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
