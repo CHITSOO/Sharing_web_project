70be2c113b22ab0ff542772986b5ebd798042bed
CHITSOO <jisu3148496@naver.com>
2021-03-27 05:42:27 +0900

    final

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { auth } = require('./middleware/auth')
-const { User } = require("./config/models/User");
+const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
@@ -19,22 +19,17 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!천사와아이들화이팅!!!!')
-})
+app.get('/', (req, res) => res.send('Hello World!~~ '))
 
-app.get('/api/hello', (req, res) => {
-  res.send("안녕하세요 ~")
-})
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
-app.post('/api/users/register', (req, res) => {
-
-  //회원가입 할 때 필요한 정부들을 client에서 가져오면
+app.post('/api/users//register', (req, res) => {
+  
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
-
   const user = new User(req.body)
 
-  user.save((err,userInfo) => {
+  user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
@@ -45,7 +40,6 @@ app.post('/api/users/register', (req, res) => {
 app.post('/api/users/login', (req, res) => {
   
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
       return res.json({
@@ -57,7 +51,7 @@ app.post('/api/users/login', (req, res) => {
   //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch)
-        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
 
         //비밀번호 까지 맞다면 토큰을 생성하기.
         user.generateToken((err, user) => {
@@ -72,9 +66,9 @@ app.post('/api/users/login', (req, res) => {
   })  
 })
 
+//role 1 어드민    role 2 특정 부서 어드민
 //role 0 ->일반유저 role 0이 아니면 관리자 
 app.get('/api/users/auth', auth , (req, res) => {
-
   //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
   res.status(200).json({
     _id: req.user._id,
@@ -89,7 +83,7 @@ app.get('/api/users/auth', auth , (req, res) => {
 })
 
 app.get('/api/users/logout', auth, (req, res) => {
-  console.log('req.user', req.user)
+  //console.log('req.user', req.user)
   User.findOneAndUpdate({ _id: req.user._id },
     { token: "" }
     , (err, user) => {
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,4 @@
-const { User } = require("../config/models/User");
-
+const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
     //인증처리를 하는 곳
@@ -8,8 +7,8 @@ let auth = (req, res, next) => {
 
     //토큰을 복호화 한후 유저를 찾는다.
     User.findByToken(token, (err, user) => {
-        if(err) throw err;
-        if(!user) return res.json({ isAuth: false, error: true })
+        if (err) throw err;
+        if (!user) return res.json({ isAuth: false, error: true })
 
         req.token = token;
         req.user = user;
